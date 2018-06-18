8d1f72fd5de6bf902baeb1dafe29311a0c1290a5
Julian Herrera <julian.herrera@live.com>
2018-06-18 16:42:43 -0500

    add prettier.js

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-source-names-using-follow (quote ("Imenu")))
  '(package-selected-packages
-	 (quote
-		(hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode geiser markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package))))
+   (quote
+    (prettier-js hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode geiser markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

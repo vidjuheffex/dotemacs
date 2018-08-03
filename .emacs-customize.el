b11528123355de9988d311efb8bffc599fcb9212
Julian Herrera <julian.herrera@live.com>
2018-08-03 09:23:37 -0500

    add nyan-mode

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -3,12 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
+   (quote
+    ("9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
  '(geiser-active-implementations (quote (sagittarius)))
  '(geiser-default-implementation (quote saggitarius-scheme))
  '(helm-source-names-using-follow (quote ("Imenu")))
+ '(hl-sexp-background-color "#efebe9")
  '(package-selected-packages
    (quote
-    (htmlize gnuplot gnuplot-mode ob-browser ob-html simple-httpd prettier-js hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package))))
+    (nyan-mode htmlize gnuplot gnuplot-mode ob-browser ob-html simple-httpd prettier-js hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

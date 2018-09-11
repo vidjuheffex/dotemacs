fc7978d056a1c004f4ecee1298baf0b8e0b01a6c
Julian Herrera <julian.herrera@live.com>
2018-09-11 15:45:30 -0500

    new theme new font

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -6,21 +6,23 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["#ffffff" "#183691" "#969896" "#a71d5d" "#969896" "#969896" "#795da3" "#969896"])
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(company-quickhelp-color-background "#e8e8e8")
+ '(company-quickhelp-color-foreground "#444444")
  '(custom-safe-themes
    (quote
-    ("8d373c184700a2c7020820dbeb098662f64bf755381d5fccdfe49bdede8ecb7e" "d5aec3a39364bc4c6c13f472b2d0cdaebd5cff7a6e4839749be2156fcc075006" "75a40e783d6b4b8f7a0aa3c7af4543e7a5b1f92b750095726442d9a00142eacc" "5057614f7e14de98bbc02200e2fe827ad897696bfd222d1bcab42ad8ff313e20" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
+    ("88fe886062e11becef4fc9beaa87bd202e6e41563bb7bf750eb04a174b080223" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "679ee3b86b4b34661a68ba45bbd373eab0284caee6249139b2a090c9ddd35ce0" "e2e6ffe55113dfba8545db9722bc5235a03eee1e8c56b5edad8c4c5d04ae122e" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "8d373c184700a2c7020820dbeb098662f64bf755381d5fccdfe49bdede8ecb7e" "d5aec3a39364bc4c6c13f472b2d0cdaebd5cff7a6e4839749be2156fcc075006" "75a40e783d6b4b8f7a0aa3c7af4543e7a5b1f92b750095726442d9a00142eacc" "5057614f7e14de98bbc02200e2fe827ad897696bfd222d1bcab42ad8ff313e20" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" default)))
  '(fci-rule-color "#969896")
  '(geiser-active-implementations (quote (sagittarius)))
  '(geiser-default-implementation (quote saggitarius-scheme))
- '(helm-source-names-using-follow (quote ("Imenu")))
+ '(helm-source-names-using-follow (quote ("Projectile files" "Imenu")))
  '(hl-sexp-background-color "#efebe9")
  '(nrepl-message-colors
    (quote
     ("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896")))
  '(package-selected-packages
    (quote
-    (paper-theme chyla-theme habamax-theme github-theme nyan-mode htmlize gnuplot gnuplot-mode ob-browser ob-html simple-httpd prettier-js hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package)))
+    (monotropic-theme leuven org-bullets org-variable-pitch moe-theme sunburn-theme doneburn-theme ample-theme paper-theme chyla-theme habamax-theme github-theme nyan-mode htmlize gnuplot gnuplot-mode ob-browser ob-html simple-httpd prettier-js hydra olivetti olivetti-mode restclient company-restclient graphql-mode switch-buffer-functions ob-mongo fountain-mode company-lua lua-mode markdown-mode company-jedi python-mode rainbow-mode emmet-mode company-web web-mode json-mode indium tide add-node-modules-path js2-refactor rjsx-mode js2-mode eslint-fix company-quickhelp company toc-org ob-restclient diff-hl gitignore-mode git-link magit helm-dash helm-ag helm-fuzzier helm-flx helm-projectile helm-smex helm-swoop helm rainbow-delimiters diminish expand-region projectile smartparens visual-regexp exec-path-from-shell duplicate-thing yasnippet adaptive-wrap flycheck multiple-cursors ace-window dedicated smex wgrep editorconfig ibuffer-sidebar dired-sidebar dired-subtree undo-tree golden-ratio-scroll-screen leuven-theme use-package)))
  '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
  '(vc-annotate-background "#b0cde7")
  '(vc-annotate-color-map

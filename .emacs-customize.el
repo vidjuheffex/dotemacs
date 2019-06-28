0ad72cfcd9999202872d6b03450c47b2232b91b1
julian herrera <julian.herrera@live.com>
2019-06-28 17:07:53 -0500

    clean up customize and add some httpd helpers

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -3,63 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(compilation-message-face 'default)
- '(custom-safe-themes
-   '("5b52a4d0d95032547f718e1574d3a096c6eaf56117e188945ae873bdb3200066" "fa2af0c40576f3bde32290d7f4e7aa865eb6bf7ebe31eb9e37c32aa6f4ae8d10" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f738c3eb5cfc7e730fea413f9cd8ba0624bd8b4837451660fe169f13f77c7814" "19b9349a6b442a2b50e5b82be9de45034f9b08fa36909e0b1be09433234610bb" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "b264fb24d37564511ca04b2fdcc2478fd414aef96b30489af6bb5c320669b093" default))
- '(fci-rule-color "#383838")
- '(frame-background-mode 'dark)
- '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
- '(highlight-tail-colors
-   '(("#3C3D37" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#3C3D37" . 100)))
- '(lsp-auto-guess-root t)
- '(lsp-prefer-flymake :none)
- '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(package-selected-packages
-   '(dracula-theme js-doc ob-typescript company-box monokai-theme zenburn-theme wgrep web-mode visual-regexp use-package undo-tree toc-org tide tao-theme switch-buffer-functions smartparens rjsx-mode rainbow-mode python-mode prettier-js plantuml-mode plain-theme olivetti ob-restclient ob-browser nyan-mode nova-theme monotropic-theme moe-theme magit lsp-ui leuven-theme lenlen-theme lab-themes json-mode indium ibuffer-sidebar hydra hydandata-light-theme htmlize helm-swoop helm-smex helm-projectile helm-fuzzier helm-flx helm-dash helm-ag graphql-mode golden-ratio-scroll-screen gnuplot-mode gnuplot gitignore-mode git-link geiser fountain-mode expand-region exec-path-from-shell eslint-fix emmet-mode editorconfig duplicate-thing doneburn-theme dired-sidebar diminish diff-hl dedicated dakrone-light-theme company-web company-restclient company-quickhelp company-lua company-lsp company-jedi cider add-node-modules-path adaptive-wrap ace-window))
- '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
- '(pos-tip-background-color "#FFFACE")
- '(pos-tip-foreground-color "#272822")
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   '((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3")))
- '(vc-annotate-very-old-color "#DC8CC3")
- '(weechat-color-list
-   '(unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+   '(calfw-ical calfw impatient-mode simple-httpd zenburn-theme wgrep web-mode visual-regexp use-package undo-tree toc-org tide tao-theme switch-buffer-functions smartparens rjsx-mode rebecca-theme rainbow-mode python-mode prettier-js plantuml-mode plain-theme olivetti ob-typescript ob-restclient ob-browser nyan-mode nova-theme monotropic-theme monokai-theme moe-theme magit-popup magit lsp-ui leuven-theme lenlen-theme lab-themes json-mode js-doc indium ibuffer-sidebar hydra hydandata-light-theme htmlize helm-swoop helm-smex helm-projectile helm-fuzzier helm-flx helm-dash helm-ag graphql-mode golden-ratio-scroll-screen gnuplot-mode gnuplot gitignore-mode git-link geiser fountain-mode expand-region exec-path-from-shell eslint-fix emmet-mode editorconfig duplicate-thing dracula-theme doneburn-theme dired-sidebar diminish diff-hl dedicated dakrone-light-theme company-web company-restclient company-quickhelp company-lua company-lsp company-jedi company-box cider challenger-deep-theme add-node-modules-path adaptive-wrap ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282a36" :foreground "#f8f8f2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Operator Mono"))))
- '( )
- )
+ '(default ((t (:family "Operator Mono")))))

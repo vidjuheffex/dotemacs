e7b453c87f678e53d18964ed58fc69d14146789e
julian herrera <julian.herrera@live.com>
2019-07-31 13:56:32 -0500

    better scheme, yaml support. disable company for helm-company

--- .emacs-customize.el
+++ .emacs-customize.el
@@ -4,10 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(calfw-ical calfw impatient-mode simple-httpd zenburn-theme wgrep web-mode visual-regexp use-package undo-tree toc-org tide tao-theme switch-buffer-functions smartparens rjsx-mode rebecca-theme rainbow-mode python-mode prettier-js plantuml-mode plain-theme olivetti ob-typescript ob-restclient ob-browser nyan-mode nova-theme monotropic-theme monokai-theme moe-theme magit-popup magit lsp-ui leuven-theme lenlen-theme lab-themes json-mode js-doc indium ibuffer-sidebar hydra hydandata-light-theme htmlize helm-swoop helm-smex helm-projectile helm-fuzzier helm-flx helm-dash helm-ag graphql-mode golden-ratio-scroll-screen gnuplot-mode gnuplot gitignore-mode git-link geiser fountain-mode expand-region exec-path-from-shell eslint-fix emmet-mode editorconfig duplicate-thing dracula-theme doneburn-theme dired-sidebar diminish diff-hl dedicated dakrone-light-theme company-web company-restclient company-quickhelp company-lua company-lsp company-jedi company-box cider challenger-deep-theme add-node-modules-path adaptive-wrap ace-window)))
+   '(yaml-mode helm-company forge magithub calfw-ical calfw impatient-mode simple-httpd zenburn-theme wgrep web-mode visual-regexp use-package undo-tree toc-org tide tao-theme switch-buffer-functions smartparens rjsx-mode rebecca-theme rainbow-mode python-mode prettier-js plantuml-mode plain-theme olivetti ob-typescript ob-restclient ob-browser nyan-mode nova-theme monotropic-theme monokai-theme moe-theme magit-popup magit lsp-ui leuven-theme lenlen-theme lab-themes json-mode js-doc indium ibuffer-sidebar hydra hydandata-light-theme htmlize helm-swoop helm-smex helm-projectile helm-fuzzier helm-flx helm-dash helm-ag graphql-mode golden-ratio-scroll-screen gnuplot-mode gnuplot gitignore-mode git-link geiser fountain-mode expand-region exec-path-from-shell eslint-fix emmet-mode editorconfig duplicate-thing dracula-theme doneburn-theme dired-sidebar diminish diff-hl dedicated dakrone-light-theme company-web company-restclient company-quickhelp company-lua company-lsp company-jedi company-box cider challenger-deep-theme add-node-modules-path adaptive-wrap ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Operator Mono")))))
+ )
